5db8ecbd8f7e799c67339a2ae887767b8ba6313d
ANUMADHAVAN02 <146543040+ANUMADHAVAN02@users.noreply.github.com>
2023-10-01 14:08:24 +0530

    uploading my projects to github repository

---
+++ SampleForloopDecrement.java
@@ -0,0 +1,20 @@
+class SampleForloopDecrement
+{
+public static void main(String args[])
+{
+for(int i=10;i>=0;i--)
+
+{
+System.out.println(i);
+}
+}
+}
+
+
+
+/*i=10;i>5,10>0 T, PRINT i-10;i--=9;
+i=9;9>5 T,Print i=9;i--=8
+i=8;8>5 T ;Print i=8;i--=7
+i=7;7>5 T;print i=7;i--=6
+i=6;6>5 T;print i=6;i--=5
+i=5;5>=5 T;Print i=5;i--4*/
---
+++ SwitchSample.java
@@ -0,0 +1,26 @@
+class SwitchSample
+{
+ 
+ public static void main(String args[])
+ {
+ int n=3;
+ switch(n)
+ {
+	 case 1:
+	 System.out.println("number is one");
+	 break;
+	 case 3:
+	 System.out.println("Number is three");
+	// break;
+	 case 2:
+	 System.out.println("Number is two");
+	 break;
+	 default:
+	 System.out.println("Invalid");
+ }
+	 
+ }
+ 
+ 
+ 
+}
